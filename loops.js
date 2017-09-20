5c1212f2edeefe796e03c44e46e570ad46b2b1fe
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-20 22:27:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,33 +2,33 @@
 
 
 function forLoop(array){
-  for (let i = 0; i < 25; i++) {
+  /*for (let i = 0; i < 25; i++) {
     if i === 1 {
       console.log("I am 1 strange loop.")
     } else {
       console.log(`I am ${i} strange loops.`)
     }
   }
-  return array
+  return array*/
 }
 
 
 
 
 function whileLoop(n) {
-
+/*
   while (n > 0) {
     console.log(n)
     n--
   }
-  return "done"
+  return "done"*/
 
 }
 
 
 
 function doWhileLoop(array){
-
+/*
   function maybeTrue() {
     return Math.random() >= 0.5
   }
@@ -37,5 +37,5 @@ function doWhileLoop(array){
     array.pop()
   } while ( array.length > 0 or maybeTrue()  );
   return array
-
+*/
 }
