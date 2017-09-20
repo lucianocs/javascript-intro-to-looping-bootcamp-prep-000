9c2929ccbaae4227fe8e884c5ba3d5e01c7dc8f7
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-20 22:24:00 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,6 +19,23 @@ fuunction whileLoop(n) {
 
 }
 
+
+
 function doWhileLoop(array){
+
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+   
+  do {
+    array.pop()
+  } while ( array.length > 0 or maybeTrue()  );
+
+
+
+
+
+
+
 
 }
